e6b725671dbfdff6ecb25ece4092ed117de93942
Kyrylo Silin <kyrylosilin@gmail.com>
2013-08-17 17:40:11 +0300

    Include Marionette.js
    I want to start everything from scratch, since rewriting will take too
    much time.
    
    Don't include the existing JS files.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,15 @@
 //= require lib/jquery/jquery
-//= require util/toggle
-//= require_directory .
+//= require lib/underscore/underscore
+//= require lib/backbone/backbone
+//= require lib/marionette/lib/backbone.marionette
+
+
+// require helper
+// require config
+// require coder
+// require rules
+// require mystorage
+// require main
+// require parser
+// require render
+// require util/toggle
